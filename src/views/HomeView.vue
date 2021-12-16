a4ec3d3707a9c69a70a9871dc915eba639dc6be0
Chambers <46649021+connectshark@users.noreply.github.com>
2021-12-16 23:32:09 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-const url = `https://api.instagram.com/oauth/authorize?client_id=328160172224472&redirect_uri=https://ig-vote.onrender.com/auth/&response_type=code&state=look&scope=user_profile,user_media`
+const url = `https://api.instagram.com/oauth/authorize?client_id=328160172224472&redirect_uri=https://ig-vote.onrender.com/auth&response_type=code&state=look&scope=user_profile,user_media`
 </script>
 
 <template>
